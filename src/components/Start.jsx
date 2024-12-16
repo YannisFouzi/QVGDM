2d19dfbeef63f90269f628f514a8adc4a75a33c5
YannisFouzi <yfouzi.dev@gmail.com>
2024-12-16 05:11:59 +0100

    Refactor Start component and update styles
    - Adjusted button styles in App.css for improved appearance, including font size and transition effects.
    - Enhanced Start.jsx to manage game state with a new 'handleStartGame' function, allowing users to start the game and choose their part afterward.
    - Updated button layout to conditionally render based on game state, improving user experience.

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useSound from "use-sound";
 import game from "../assets/main.mp3";
 
 export default function Start({ setUserName, setSelectedPart }) {
   const [playGame, { stop }] = useSound(game);
+  const [gameStarted, setGameStarted] = useState(false);
 
   useEffect(() => {
-    // Jouer la musique au chargement du composant
     playGame();
-    // Arrêter la musique quand le composant est démonté
     return () => stop();
   }, [playGame, stop]);
 
@@ -18,19 +17,41 @@ export default function Start({ setUserName, setSelectedPart }) {
     setSelectedPart(partNumber);
   };
 
+  const handleStartGame = () => {
+    setGameStarted(true);
+  };
+
   return (
     <div className="start">
       <div className="content">
         <div className="wrapper">
-          <h2 className="title">Choisissez votre partie</h2>
-          <div className="btn">
-            <button className="startButton" onClick={() => handleClick(1)}>
-              Damien & Lucca
-            </button>
-            <button className="startButton" onClick={() => handleClick(2)}>
-              Julien & Steven
-            </button>
-          </div>
+          {!gameStarted ? (
+            <div>
+              <h2 className="title">
+                Qui veut gagner des millions de ballons ?
+              </h2>
+              <div className="btn">
+                <button
+                  className="startButton full-width"
+                  onClick={handleStartGame}
+                >
+                  COMMENCER PARTIE
+                </button>
+              </div>
+            </div>
+          ) : (
+            <div>
+              <h2 className="title">Choisissez votre partie</h2>
+              <div className="btn">
+                <button className="startButton" onClick={() => handleClick(1)}>
+                  Damien & Lucca
+                </button>
+                <button className="startButton" onClick={() => handleClick(2)}>
+                  Julien & Steven
+                </button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
